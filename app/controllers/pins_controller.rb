5043f32103086a99d246f2c17dbc48e5e745387a
Quentin Pradelle <quentinpradelle@icloud.com>
2015-09-18 14:58:04 +0200

    edited pins views and controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,25 +1,20 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /pins
   def index
     @pins = Pin.all
   end
 
-  # GET /pins/1
   def show
   end
 
-  # GET /pins/new
   def new
     @pin = Pin.new
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
   def create
     @pin = Pin.new(pin_params)
 
@@ -30,7 +25,6 @@ class PinsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pins/1
   def update
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
@@ -39,19 +33,16 @@ class PinsController < ApplicationController
     end
   end
 
-  # DELETE /pins/1
   def destroy
     @pin.destroy
     redirect_to pins_url, notice: 'Pin was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def pin_params
       params.require(:pin).permit(:description)
     end
